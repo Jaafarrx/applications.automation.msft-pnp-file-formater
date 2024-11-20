f6458d3933f9dc9d2af9cd7b7f83da2e6e1e864b
Jaafarrx <164268658+Jaafarrx@users.noreply.github.com>
2024-11-20 14:37:00 -0800

    feat(directory_formater): Project setup
    Project setup to start development

---
+++ directory_formater.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+import pathlib
+
+def directory_formater() -> bool:
+    return False
+
+
+if __name__  == "__main__":
+    result = directory_formater()
+    if result:
+        print("Conversion Succeeded!")
+    else:
+        print("Conversion Failed!")
